7203344d8802aa902a03fd1967355acfda925207
TAYZAR TUN <6631503089@lamduan.mfu.ac.th>
2025-01-20 23:18:47 +0700

    1.1 modify Poster,User,RequestClubLeader

---
+++ src/main/java/com/project/idiotclub/app/entity/RequestClubLeader.java
@@ -0,0 +1,11 @@
+package com.project.idiotclub.app.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class RequestClubLeader {
+
+    @Id
+    private int id;
+}
--- src/main/java/com/project/idiotclub/app/entity/User.java
+++ src/main/java/com/project/idiotclub/app/entity/User.java
@@ -1,4 +1,31 @@
 package com.project.idiotclub.app.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import org.springframework.web.multipart.MultipartFile;
+
+@Entity
 public class User {
+
+    @Id
+    @Column(name = "user_id")
+    private int id;
+
+    @Column(name="user_name", updatable = false,nullable = false)
+    private String name;
+
+    @Column(name="user_password",updatable = false,nullable = false)
+    private String password;
+
+    @Column(name="user_email",updatable = false,nullable = false)
+    private String email;
+
+    @Column(name="user_profile_image",updatable = true,nullable = true)
+    private String profile_image;
+
+    @Column(name="user_role",updatable = true,nullable = false)
+    private Role role;
+
+
 }
