5c5f1e9b4ad4235e920b10d6024dac02f2f4bd3f
paung29 <aikepaung767@gmail.com>
2025-04-11 15:57:06 +0700

    2.9 paung ()

---
+++ src/main/java/com/project/idiotclub/app/util/community/ViewJoinCommunityRequestOutputForm.java
@@ -0,0 +1,12 @@
+package com.project.idiotclub.app.util.community;
+
+import lombok.Data;
+
+@Data
+public class ViewJoinCommunityRequestOutputForm {
+	
+	private String userName;
+	private String userPhoto;
+	private Long userId;
+
+}
